52abc76120ac32fea681a37a23ee49fb45d0fa74
mutao <mutao.net@gmail.com>
2025-02-21 01:02:27 +0900

    del: didabled property

--- pomodoro-timer/src/index.tsx
+++ pomodoro-timer/src/index.tsx
@@ -42,7 +42,6 @@ export default function Command() {
           <Action.SubmitForm
             title="タイマーを開始"
             onSubmit={() => startTimer()}
-            disabled={isTimerRunning}
           />
         </ActionPanel>
       }
